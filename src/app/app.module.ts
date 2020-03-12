26b068518f8b8c1a4d9eb20acceee01ff76ae760
Gustavo Herrera <tavoxr23@gmail.com>
2020-03-11 23:49:07 -0600

    added bootswatch/lux and navigation, preview, photo-form/list components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { PhotoFormComponent } from './components/photo-form/photo-form.component';
+import { PhotoPreviewComponent } from './components/photo-preview/photo-preview.component';
+import { PhotoListComponent } from './components/photo-list/photo-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    PhotoFormComponent,
+    PhotoPreviewComponent,
+    PhotoListComponent
   ],
   imports: [
     BrowserModule,
